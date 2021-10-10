1a7b307f0654febcebcaa2bb32749bf922e48c1c
shield44 <motoecondorxt1022@gmail.com>
2021-10-10 09:39:51 +0530

    olivewood: fix vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,3 @@
-# Use PROD Camera Pixel Format
-cd hardware/qcom-caf/msm8996/display
-git fetch https://github.com/PixelExperience/hardware_qcom-caf_msm8996_display
-git cherry-pick 20a6dc8b24853cffda07e3b454503740bac79a4d
-cd ..
-# Use LineageOS audio hals
-rm -rf audio
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8996 audio
-cd ../../../
+# Track our display hals
+rm -rf hardware/qcom-caf/msm8996-los/display
+git clone https://github.com/mi-sdm439/android_hardware_qcom_display -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996-los/display
